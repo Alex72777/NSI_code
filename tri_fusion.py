f54ceb6dd507fcafadd5439a0597c04afcd8f32b
eleve <alex2008.cauvet@gmail.com>
2026-02-05 17:08:24 +0100

    tri fusion

--- tri_fusion.py
+++ tri_fusion.py
@@ -1,9 +1,21 @@
 
 
-def fusion(gauche: list, droite: list) -> list:
-    pass
+def regner(gauche: list, droite: list) -> list:
+    final = []
+    while len(gauche) > 0 or len(droite) > 0:
+        if len(gauche) == 0:
+            final.append(droite.pop(0))
+        elif len(droite) == 0:
+            final.append(gauche.pop(0))
+        elif gauche[0] < droite[0]:
+            final.append(gauche.pop(0))
+        else:
+            final.append(droite.pop(0))
+#     print(final)
+    return final
+    
 
-def tri_fusion(liste: list[float]) -> list[float]:
+def diviser_regner(liste: list[float]) -> list[float]:
     """
     Docstring
     """
@@ -11,12 +23,16 @@ def tri_fusion(liste: list[float]) -> list[float]:
     if len(liste) <= 1:
         return liste
     
-    a = []
-    length = len(a)
-    milieu = length//2
-    a.append(liste[:milieu])
-    a.append(liste[milieu+1:])
-    print(a)
+    milieu = len(liste)//2
+    l_gauche = liste[:milieu]
+    l_droite = liste[milieu:]
+    #print(l_gauche, l_droite)
+    gauche = diviser_regner(l_gauche)
+    droite = diviser_regner(l_droite)
+    
+    return regner(gauche, droite)
+    
 
-
-tri_fusion([1,4,2,5,3])
+print(diviser_regner([]))
+print(diviser_regner([0, 45, 17, 999, 36]))
+# assert diviser_regner(([0, 45, 17, 999, 36]) = [0,36,45,999]
